a44ad06e81944df0b0ff20a4c6d3602b6b663858
yabool2001 <mzemlo.pl@gmail.com>
2024-08-20 12:54:15 -1000

    matplotlib working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Remarks:
 # Git remote respository doesn't contain CVS folders: Mono_BLOS_19_all_in_one-main_ael/ and cemeouhf__ael/ You can find them on Google Drive https://drive.google.com/drive/folders/1VwieRbJuh2ZwxqC4sYGRUO-VsWpSQx9H
 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 from pathlib import Path
 
@@ -51,45 +53,42 @@ print ( df.loc[df['Elevation (deg)'] == max_elevation] , 'Time (UTCG)' )
 # print ( df.sort_values ( by = 'Elevation (deg)' , ascending = False ) )
 
 df_filtered = df[ df.groupby ( 'Time (UTCG)' )[ 'Time (UTCG)' ].transform ( 'size' ) > 1 ]
-print ( df_filtered )
-print ( df )
-df.to_csv('results/df.csv', index = False )
-
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+# print ( df_filtered )
+# print ( df )
+# df.to_csv('results/df.csv', index = false )
 
 # Ustawienie dłuższego wykresu (panoramicznego)
 # Filtracja danych dla okresu styczeń 2000
-start_date = '2000-01-01'
-end_date = '2000-01-31'
-df_january = df [ ( df['Time (UTCG)'] >= start_date ) & ( df[ 'Time (UTCG)' ] <= end_date ) ]
+start_date = pd.to_datetime ( '2000-01-01' )
+end_date = pd.to_datetime ( '2000-01-31' )
+df_january = df[ ( df[ 'Time (UTCG)' ] >= start_date ) & ( df[ 'Time (UTCG)' ] <= end_date ) ]
 
 # Zdefiniowanie unikalnych nazw satelitów
-satellites = df_january['To satellite'].unique()
+satellites = df_january[ 'To satellite' ].unique ()
 
-fig, ax = plt.subplots(figsize=(20, 8))
+fig, ax = plt.subplots ( figsize = ( 20 , 8 ) )
 
 for satellite in satellites:
     df_sat = df_january[ df_january[ 'To satellite' ] == satellite ]
     ax.plot ( df_sat[ 'Time (UTCG)' ] , df_sat[ 'Elevation (deg)' ] , label = satellite )
 
 # Dostosowanie osi czasu z podziałką dzienną
-ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-plt.xticks(rotation=45)
+ax.xaxis.set_major_locator ( mdates.DayLocator ( interval = 1 ) )
+ax.xaxis.set_major_formatter ( mdates.DateFormatter ( '%Y-%m-%d' ) )
+plt.xticks ( rotation = 45 )
 
 # Ustawienie początkowego zakresu na kilka dni (np. pierwsze 3 dni stycznia 2000)
-start_view = '2000-01-01'
-end_view = '2000-01-03'
-ax.set_xlim([start_view, end_view])
+start_view = pd.to_datetime ( '2000-01-01' )
+end_view = pd.to_datetime ( '2000-01-03' )
+ax.set_xlim ( [ start_view , end_view ] )
 
 # Dodanie legendy, siatki i opisów osi
-plt.xlabel('Czas (UTCG)')
-plt.ylabel('Elewacja (deg)')
-plt.title('Elewacja satelitów w styczniu 2000')
-plt.legend(title='Satelita')
-plt.grid(True)
+plt.xlabel ( 'Czas (UTCG)' )
+plt.ylabel ( 'Elewacja (deg)' )
+plt.title ( 'Elewacja satelitów w styczniu 2000' )
+plt.legend ( title = 'Satelita' )
+plt.grid ( True )
 
 # Włączenie przewijania w osi czasu (można użyć narzędzia do powiększania w matplotlib)
-plt.tight_layout()
-plt.show()
+plt.tight_layout ()
+plt.show ()
